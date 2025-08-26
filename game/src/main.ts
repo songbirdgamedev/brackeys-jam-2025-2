f3e7c7999b1a2d5d973e9dd30ed5db7fb1e33631
emery <215300895+xexemery@users.noreply.github.com>
2025-08-26 22:36:57 +0100

    Add options and card sprites

--- game/src/main.ts
+++ game/src/main.ts
@@ -1,10 +1,21 @@
 import kaplay from "kaplay";
 
-const k = kaplay();
+const k = kaplay({
+  width: 640,
+  height: 480,
+  scale: 2,
+  stretch: false,
+  letterbox: false,
+  debug: true,
+  crisp: false,
+  texFilter: "nearest",
+  global: false,
+});
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
-
-k.onClick(() => k.addKaboom(k.mousePos()));
+// load cards sprites
+k.loadSprite("cards", "sprites/cards.png", {
+  sliceX: 14,
+  sliceY: 4,
+});
