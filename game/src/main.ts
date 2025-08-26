0cf955419acbb71c41d950c151accdb9cbafc80d
emery <215300895+xexemery@users.noreply.github.com>
2025-08-26 13:39:02 +0100

    Create project structure

---
+++ game/src/main.ts
@@ -0,0 +1,10 @@
+import kaplay from "kaplay";
+
+const k = kaplay();
+
+k.loadRoot("./"); // A good idea for Itch.io publishing later
+k.loadSprite("bean", "sprites/bean.png");
+
+k.add([k.pos(120, 80), k.sprite("bean")]);
+
+k.onClick(() => k.addKaboom(k.mousePos()));
